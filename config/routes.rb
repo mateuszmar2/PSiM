a8dd60f737c461cb3e13573afad2b7fb8ccf4504
Mateusz Marciniec <mateuszmar2@gmail.com>
2022-06-20 18:41:19 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'barrier'
+
+  post 'reservation/create'
+  patch 'reservation/update'
+  get 'reservation/list'
+  get 'reservation/show'
+  delete 'reservation/delete'
+
+  post 'user/create'
+  patch 'user/update'
+  get 'user/list'
+  get 'user/show'
+  delete 'user/delete'
+
+  post 'spot/create'
+  patch 'spot/update'
+  get 'spot/list'
+  get 'spot/show'
+  delete 'spot/delete'
+
 end
